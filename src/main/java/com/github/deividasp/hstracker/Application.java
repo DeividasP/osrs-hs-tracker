5e93427cda3c17bc8b9e03c89f7a885866c6344e
Deividas Popelskis <deividas.popelskis@gmail.com>
2016-12-26 18:48:37 +0200

    Spring application launcher

---
+++ src/main/java/com/github/deividasp/hstracker/Application.java
@@ -0,0 +1,16 @@
+package com.github.deividasp.hstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Deividas Popelskis <deividas.popelskis@gmail.com>
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] arguments) {
+		SpringApplication.run(Application.class, arguments);
+	}
+
+}
