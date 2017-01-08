5961695a707b0ae88a3b5e74977539a6b98cbad5
Deividas Popelskis <deividas.popelskis@gmail.com>
2017-01-08 15:54:43 +0200

    Query for finding a HighScores object by timestamp

--- src/main/java/com/github/deividasp/hstracker/repository/HighScoresRepository.java
+++ src/main/java/com/github/deividasp/hstracker/repository/HighScoresRepository.java
@@ -8,12 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 /**
  * @author Deividas Popelskis <deividas.popelskis@gmail.com>
  */
 public interface HighScoresRepository extends CrudRepository<HighScores, HighScoresPK> {
 
 	@Query("SELECT CASE WHEN COUNT(h) > 0 THEN true ELSE false END FROM HighScores h WHERE h.username = :username AND h.gameMode = :gameMode")
-	boolean existsByUsernameAndGameMode(@Param("username") String username, @Param("gameMode") GameModes gameMode);
+	boolean exists(@Param("username") String username, @Param("gameMode") GameModes gameMode);
+
+	@Query(value = "SELECT * FROM high_scores h WHERE h.username = ?1 AND h.game_mode = ?2 AND h.timestamp >= ?3 ORDER BY h.timestamp ASC LIMIT 1", nativeQuery = true)
+	Optional<HighScores> find(String username, String gameMode, long timestamp);
 
 }
